ac93975a11a2c84f058812dce406f966dee5545e
Umberto Pasinetti <pasinetti96@gmail.com>
2024-10-31 13:42:43 +0100

    fix base & toml

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://egesiapres.github.io/subEditor-app/",
+  base: "./",
   plugins: [nodePolyfills(), react()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
